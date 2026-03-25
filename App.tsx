260947c2dde352ee075af6ee44d00a99d39deeea
Niklas <niklas.kamarainen@gmail.com>
2026-03-25 13:47:24 +0200

    ulkoasu kirjautumiselle ja rekisteröitymiselle

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import SignIn from './screens/SignIn'
+import SignUp from './screens/SignUp'
+
 export default function App() {
   return (
     <View style={styles.container}>
